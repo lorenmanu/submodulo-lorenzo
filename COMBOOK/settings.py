8d434be2dc9bfdc11443c9970614b796278ca39f
loremanu <lorenrr1@gmail.com>
2015-11-28 15:43:48 +0100

    Añadiendo

--- COMBOOK/settings.py
+++ COMBOOK/settings.py
@@ -63,12 +63,20 @@ MEDIA_URL = 'http://localhost:8000/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-import os
-ROOT_PATH = os.path.dirname(__file__)
-STATIC_ROOT = os.path.join(ROOT_PATH, 'estaticos')
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/estaticos/'
+
+
+# Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(ROOT_PATH, 'estaticos'),
+    RUTA_PROYECTO.child('estaticos'),
+ 
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
