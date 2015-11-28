46efa8c3c93597464f7d50e0b79e1bcd8ea1bba0
loremanu <lorenrr1@gmail.com>
2015-11-28 15:05:35 +0100

    Añadiendo

--- COMBOOK/settings.py
+++ COMBOOK/settings.py
@@ -63,11 +63,11 @@ MEDIA_URL = 'http://localhost:8000/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'http://localhost:8000/media/'
+STATIC_ROOT = 'http://localhost:8000/estaticos/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = 'http://localhost:8000/estaticos/'
+STATIC_URL = 'http://localhost:8000/'
 
 
 # Additional locations of static files
