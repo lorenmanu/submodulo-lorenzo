452d9cb3a446340706c3fe32e0b152cfaa64fc21
loremanu <lorenrr1@gmail.com>
2015-11-30 11:38:48 +0100

    Añadiendo

--- desplieque.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/lorenmanu/submodulo-lorenzo.git
-cd submodulo-lorenzo
-pwd
-heroku keys:add
-heroku git:clone -a combook
-heroku ps:scale web=1 --app combook
-heroku open --app combook
