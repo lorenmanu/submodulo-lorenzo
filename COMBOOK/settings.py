2f06220888f99fc4233308fcb13dacefb7943a52
loremanu <lorenrr1@gmail.com>
2015-12-01 14:46:37 +0100

    Añadiendo

--- COMBOOK/settings.py
+++ COMBOOK/settings.py
@@ -159,6 +159,7 @@ ON_HEROKU = os.environ.get('PORT')
 if ON_HEROKU:
     DATABASE_URL=' postgres://qcgsjyjlrxrbut:AB6HfA2cXIV74B8z_xKl-V88vI@ec2-107-21-219-109.compute-1.amazonaws.com:5432/df1bb3foip112r'
     DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+    PORT:'8000'
 
 
 DATABASES = {
