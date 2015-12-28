4a465f2ac20437a78a7d8c47acb3d706c94e8b08
loremanu <lorenrr1@gmail.com>
2015-12-28 13:48:08 +0100

    Añadiendo

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'MiTienda.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': os.path.join(BASE_DIR, 'templates'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -182,10 +182,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
 
 
