b274bcd4ac9289fdfd1d8cec3f81ce87f1993b3b
loremanu <lorenrr1@gmail.com>
2016-01-02 19:13:29 +0100

    Añdiendo

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+
 #Actualizar Sistema Base
-echo "actualizar sistema base"
 sudo apt-get -y update
 
+#Descargar aplicacion
+sudo apt-get install -y git
+sudo git clone https://github.com/lorenmanu/submodulo-lorenzo.git
+
 # Instalar Python y PostgreSQL
-echo "Instalar dependencias"
 sudo apt-get install -y python-setuptools
+sudo apt-get -y build-dep python-imaging --fix-missing
 sudo apt-get -y install python-dev
 sudo apt-get -y install build-essential
 sudo apt-get -y install python-psycopg2
 sudo apt-get -y install libpq-dev
+sudo apt-get install python2.7
 sudo easy_install pip
-sudo pip install --upgrade pip
+sudo easy_install Pillow
 
-#Instalamos la aplicacion
-echo "instalar requirements"
-cat requirements.txt
+#Instalar la app
 sudo pip install -r requirements.txt
